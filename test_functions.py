4e8f28b4a423a64c09560843dcbd2142135d4e6e
erickguerra22 <erickguerra2201@gmail.com>
2025-09-10 17:32:15 -0600

    Conteo de vocales

--- test_functions.py
+++ test_functions.py
@@ -6,3 +6,9 @@ def test_reverse():
     assert fn.reverse("a") == "a"
     with pytest.raises(TypeError):
         fn.reverse(123)
+        
+def test_count_vowels():
+    assert fn.count_vowels("hola") == 2
+    assert fn.count_vowels("xyz") == 0
+    with pytest.raises(TypeError):
+        fn.count_vowels(None)
